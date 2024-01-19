8bd5d91c84631d668d9b1d7f2fe44fd62f840a8f
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-19 23:11:46 +0400

    added useState and useEffect for background rendering

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,57 +1,71 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import useMediaQuery from './components/shared/hooks/UseMediaQuery';
+import { useEffect, useState } from 'react';
 
 const Root = () => {
   const { pathname } = useLocation();
-  console.log('🚀 ~ Root ~ pathname:', pathname);
+  const [bg, setBg] = useState('');
+  const [device, setDevice] = useState('');
   const isDesktopMode = useMediaQuery('(min-width: 1024px)');
   const isMobileMode = useMediaQuery('(max-width: 767px)');
 
-  const device = isDesktopMode ? 'desktop' : isMobileMode ? 'mobile' : 'tablet';
-  let bg;
-  switch (pathname) {
-    case '/space-tourism':
-      if (device === 'desktop') {
-        bg = 'bg-home-desktop';
-      } else if (device === 'mobile') {
-        bg = 'bg-home-mobile';
-      } else {
-        bg = 'bg-home-tablet';
-      }
-      break;
-    case '/space-tourism/destination':
-      if (device === 'desktop') {
-        bg = 'bg-destination-desktop';
-      } else if (device === 'mobile') {
-        bg = 'bg-destination-mobile';
-      } else {
-        bg = 'bg-destination-tablet';
-      }
-      break;
-    case '/space-tourism/crew':
-      if (device === 'desktop') {
-        bg = 'bg-crew-desktop';
-      } else if (device === 'mobile') {
-        bg = 'bg-crew-mobile';
-      } else {
-        bg = 'bg-crew-tablet';
-      }
-      break;
-    case '/space-tourism/technology':
-      if (device === 'desktop') {
-        bg = 'bg-technology-desktop';
-      } else if (device === 'mobile') {
-        bg = 'bg-technology-mobile';
-      } else {
-        bg = 'bg-technology-tablet';
-      }
-      break;
-    default: {
-      bg = 'bg-home-desktop';
-      break;
+  useEffect(() => {
+    if (isDesktopMode) {
+      setDevice('desktop');
     }
-  }
+    if (isMobileMode) {
+      setDevice('mobile');
+    }
+    if (!isDesktopMode && !isMobileMode) {
+      setDevice('tablet');
+    }
+  }, [isDesktopMode, isMobileMode]);
+
+  useEffect(() => {
+    switch (pathname) {
+      case '/space-tourism':
+        if (device === 'desktop') {
+          setBg('bg-home-desktop');
+        } else if (device === 'mobile') {
+          setBg('bg-home-mobile');
+        } else {
+          setBg('bg-home-tablet');
+        }
+        break;
+      case '/space-tourism/destination':
+        if (device === 'desktop') {
+          setBg('bg-destination-desktop');
+        } else if (device === 'mobile') {
+          setBg('bg-destination-mobile');
+        } else {
+          setBg('bg-destination-tablet');
+        }
+        break;
+      case '/space-tourism/crew':
+        if (device === 'desktop') {
+          setBg('bg-crew-desktop');
+        } else if (device === 'mobile') {
+          setBg('bg-crew-mobile');
+        } else {
+          setBg('bg-crew-tablet');
+        }
+        break;
+      case '/space-tourism/technology':
+        if (device === 'desktop') {
+          setBg('bg-technology-desktop');
+        } else if (device === 'mobile') {
+          setBg('bg-technology-mobile');
+        } else {
+          setBg('bg-technology-tablet');
+        }
+        break;
+      default: {
+        setBg('bg-home-desktop');
+        break;
+      }
+    }
+  }, [pathname, device]);
 
   return (
     <div className={`${bg} bg-cover bg-center text-white min-h-screen`}>
