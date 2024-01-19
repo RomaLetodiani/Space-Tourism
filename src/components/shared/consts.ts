aaa05908885e626169d71ed830dade86faf4f987
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-20 03:44:34 +0400

    added consts file

---
+++ src/components/shared/consts.ts
@@ -0,0 +1,22 @@
+export const header = [
+  {
+    title: 'HOME',
+    link: '/',
+    index: '00',
+  },
+  {
+    title: 'DESTINATION',
+    link: '/destination',
+    index: '01',
+  },
+  {
+    title: 'CREW',
+    link: '/crew',
+    index: '02',
+  },
+  {
+    title: 'TECHNOLOGY',
+    link: '/technology',
+    index: '03',
+  },
+];
