c28eaa008548db65b34c2539bce266ba62277dce
RomaLetodiani <roman.letodiani03@gmail.com>
2024-01-22 15:42:28 +0400

    added image consts for all pages

--- src/components/shared/consts.ts
+++ src/components/shared/consts.ts
@@ -1,3 +1,20 @@
+import moon from '../../assets/destination/image-moon.png';
+import mars from '../../assets/destination/image-mars.png';
+import europa from '../../assets/destination/image-europa.png';
+import titan from '../../assets/destination/image-titan.png';
+
+import douglas from '../../assets/crew/image-douglas-hurley.png';
+import mark from '../../assets/crew/image-mark-shuttleworth.png';
+import victor from '../../assets/crew/image-victor-glover.png';
+import anouesh from '../../assets/crew/image-anousheh-ansari.png';
+
+import vehicle from '../../assets/technology/image-launch-vehicle-portrait.jpg';
+import vehicleLand from '../../assets/technology/image-launch-vehicle-landscape.jpg';
+import spaceport from '../../assets/technology/image-spaceport-portrait.jpg';
+import spaceportLand from '../../assets/technology/image-spaceport-landscape.jpg';
+import capsule from '../../assets/technology/image-space-capsule-portrait.jpg';
+import capsuleLand from '../../assets/technology/image-space-capsule-landscape.jpg';
+
 export const header = [
   {
     title: 'HOME',
@@ -20,3 +37,8 @@ export const header = [
     index: '03',
   },
 ];
+
+export const destinationImages = [moon, mars, europa, titan];
+export const crewImages = [douglas, mark, victor, anouesh];
+export const technologyImages = [vehicle, spaceport, capsule];
+export const technologyLandImages = [vehicleLand, spaceportLand, capsuleLand];
