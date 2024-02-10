561ff30be297bd5d77e2dbfeb28fbdde62f53777
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-10 07:02:27 +0400

    added spin and bouncer animations to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,21 +24,19 @@ export default {
         'home-desktop': "url('./assets/home/background-home-desktop.jpg')",
         'home-tablet': "url('./assets/home/background-home-tablet.jpg')",
         'home-mobile': "url('./assets/home/background-home-mobile.jpg')",
-        'destination-desktop':
-          "url('./assets/destination/background-destination-desktop.jpg')",
-        'destination-tablet':
-          "url('./assets/destination/background-destination-tablet.jpg')",
-        'destination-mobile':
-          "url('./assets/destination/background-destination-mobile.jpg')",
+        'destination-desktop': "url('./assets/destination/background-destination-desktop.jpg')",
+        'destination-tablet': "url('./assets/destination/background-destination-tablet.jpg')",
+        'destination-mobile': "url('./assets/destination/background-destination-mobile.jpg')",
         'crew-desktop': "url('./assets/crew/background-crew-desktop.jpg')",
         'crew-tablet': "url('./assets/crew/background-crew-tablet.jpg')",
         'crew-mobile': "url('./assets/crew/background-crew-mobile.jpg')",
-        'technology-desktop':
-          "url('./assets/technology/background-technology-desktop.jpg')",
-        'technology-tablet':
-          "url('./assets/technology/background-technology-tablet.jpg')",
-        'technology-mobile':
-          "url('./assets/technology/background-technology-mobile.jpg')",
+        'technology-desktop': "url('./assets/technology/background-technology-desktop.jpg')",
+        'technology-tablet': "url('./assets/technology/background-technology-tablet.jpg')",
+        'technology-mobile': "url('./assets/technology/background-technology-mobile.jpg')",
+      },
+      animation: {
+        spin: 'spin 5s linear infinite',
+        bouncer: 'bouncer 12s ease infinite',
       },
     },
   },
