b0415fee0e2603ff68e2f618982075ca41aad6df
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-10 07:01:04 +0400

    added Error Component

---
+++ src/components/shared/Error.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  errorMessage: string;
+};
+
+const Error = (props: Props) => {
+  return (
+    <div className="relative mt-20 w-full h-60">
+      <div className="animate-bouncer relative py-2 px-5 md:px-10 w-40 md:w-60 text-center border rounded-full">
+        <p className="text-sm md:text-xl">{props.errorMessage}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
