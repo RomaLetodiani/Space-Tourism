02dd158811ecf5ea130f99e3ca54a98c868b202e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-11 19:28:59 +0400

    built error page

--- src/Pages/error/index.tsx
+++ src/Pages/error/index.tsx
@@ -1,5 +1,16 @@
-const Error = () => {
-  return <div>Error</div>;
+import ExploreButton from '../home/ExploreButton';
+
+const ErrorPage = () => {
+  return (
+    <div className="bg-home-desktop min-w-[300px] p-5 text-white bg-cover gap-20 flex flex-col justify-center items-center min-h-screen w-full">
+      <div className="font-bellefair tracking-[5px]">
+        <h2 className="text-head2 font-barlowCondensed">Sorry</h2>
+        <h5 className="text-head3">We couldn't find that page</h5>
+        <p className="text-head4">Try the Button down below</p>
+      </div>
+      <ExploreButton />
+    </div>
+  );
 };
 
-export default Error;
+export default ErrorPage;
